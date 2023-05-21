2e9d8d12aa5b1f12f3c955b1aaa8135056e8c31a
dcinmamun <mamundc99@gmail.com>
2023-05-21 13:14:42 +0600

    if-function-loop-21-05-23

--- practice/script.js
+++ practice/script.js
@@ -8,3 +8,18 @@ const pi=3.1416;
 
 let total_area=pi*(radius*radius);
 console.log(total_area);
+
+//less then 0 very cool , 0-19 cool , 20-24 normal , 25 to above  abnormal
+let weather=-100;
+if(weather>=25){
+    console.log("abnormal");
+}
+else if(weather>=20){
+    console.log("normal");
+}
+else if(weather>=0){
+    console.log("cool");
+}
+else{
+    console.log("absolute");
+}
---
+++ practice/test-if-condition.js
@@ -0,0 +1,44 @@
+
+
+// //0-19 cool , 20-24 normal , 25 to above  abnormal
+// let weather=-31;
+// if(weather=>25){
+//     console.log("abnormal");
+// }
+// else if(weather=>20){
+//     console.log("normal");
+// }
+// else if(weather=>0){
+//     console.log("cool");
+// }
+
+// else{
+//     console.log("absolute")5/3
+// }
+
+
+// for(let x=1; x<=50; x++){
+// if(x%3==0 && x%5==0){
+//     console.log(x); 
+// }
+
+// else  { 
+// }
+// }
+
+// let input=5;
+// let result=1;
+
+// for(let i=input; i>=1;i--){
+//     result=result*i;
+// }
+// console.log(result);
+
+let input=6;
+let result=1;
+
+for(let i=input ; i>=1; i--  ){
+    result=result*i
+}
+
+console.log(result) 
