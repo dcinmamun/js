e789e0bfe73cef05b31b84bc0bc1f8123f8091ba
dcinmamun <mamundc99@gmail.com>
2023-05-22 13:08:36 +0600

    22-05-23

---
+++ practice/test-loop.js
@@ -0,0 +1,161 @@
+
+// let userName="java";
+// let password="asd123";
+
+// if (userName=="java"){
+
+//     if(password="asd123"){
+//         console.log("welcome " + java);
+//     }
+
+//     else{
+//         console.log("password is incorrect");
+//     }
+
+
+// }
+
+// else{
+    
+//     console.log("user name not found");
+// }
+
+//
+// 80-100 A+ , 70-79 A , 60-69 A- , 50-59 B , 40-49 C , 33-39 D ,0-32 F 
+// let mark=90;
+
+
+// if(mark<0){
+// console.log("N/A");
+// }
+// else if(mark<=32){
+//          console.log("F");
+//           }
+
+// else if(mark<=39){
+//         console.log("D");
+//         }
+
+// else if(mark<=49){
+//          console.log("C");
+//          }
+
+//else if(mark<=59){
+//          console.log("B");
+//        }
+
+// else if(mark<=69){
+//          console.log("A-");
+//          }
+
+// else if(mark<=79){
+//          console.log("A");
+//          }
+
+// else if(mark<=100){
+//             console.log("A+");
+//            }
+// else{
+//                 console.log("invalid number");  
+//             } 
+ /////////////////
+//      let day=2;
+        
+// switch(day){
+//     case 1:
+//     console.log("saturday")
+//     break;
+
+//     case 1:
+//     console.log("sunday")
+//     break;
+
+//     case 2:
+//     console.log("tuesday")
+//     break;
+
+//     case 3:
+//     console.log("thursday")
+//     break;
+
+//     default:
+//     console.log("nothing")
+           
+//         }
+////////////////////////
+
+// let day=5;
+
+// switch(day){
+// case 1:
+//         console.log(saturday)
+// break;
+
+// case 2:
+//         console.log(aturday)
+// break;
+
+// case 3:
+//         console.log(turday)
+// break;
+
+// case 4:
+//         console.log(ourday)
+// break;
+
+// default:
+//         console.log(00000)
+
+// }
+
+/////////////////////////
+// let number=5;
+// let counter=0;
+
+// for(let i=1; i<=number ; i++){
+// if(number%i==0){
+//     counter=counter+1;
+// }
+// }
+// if(counter==2){
+// console.log("prime");
+// }
+
+
+//  else{
+//     console.log("notprime");   
+// }
+
+//swap swapping//
+
+// let x=10;
+// let y=20;
+// let temp=0;
+
+// temp=x;
+// x=y;
+// temp=y;
+
+// console.log("x= "+x+"  y="+y);
+//////////
+
+
+let n1=0;
+let n2=1;
+let sum=0;
+
+console.log(n1);
+console.log(n2);
+
+for(let i=3 ;i<=8 ;i++){
+    sum=n1+n2
+   console.log(sum);
+   n1=n2;
+   n2=sum;
+}
+
+
+
+            
+        
+    
